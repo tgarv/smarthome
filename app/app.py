648e7ecf9210d528c6544e19fb8b1752346e6533
Thomas Garver <tgarvz@gmail.com>
2019-01-29 21:25:50 +0000

    Add endpoint for logging temperature and humidity

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
 import time
 import Adafruit_DHT
+import json
+import datetime
 app = Flask(__name__)
 
 @app.route("/")
@@ -86,7 +88,29 @@ def bluetooth_audio():
     os.system("/usr/bin/irsend SEND_ONCE YAMAHA_RAV463 KEY_5")
     return "HDMI5"
 
-@app.route("/read_temp_humidity")
-def read_temp_humidity():
+@app.route("/read_temperature_humidity")
+def read_temperature_humidity():
     humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 24)
     return jsonify({"temperature": temperature, "humidity": humidity})
+
+@app.route("/log_temperature_humidity", methods=["GET", "POST"])
+def log_temperature_humidity():
+    humidity = request.args.get("humidity")
+    temperature = request.args.get("temperature")
+    room = request.args.get("room")
+    time = datetime.datetime.now()
+    new_data = {"temperature": temperature, "humidity": humidity, "room": room, "time": time.isoformat()}
+    with open("/home/pi/projects/smart_home/temp_humidity_log.txt", "r") as json_file:
+        try:
+            data = json.load(json_file)
+        except ValueError:
+            print "failed to load json data"
+            data = {"entries": []}
+        json_file.close()
+    with open("/home/pi/projects/smart_home/temp_humidity_log.txt", "w") as json_file: 
+        data["entries"].append(new_data)
+        json.dump(data, json_file)
+        json_file.close()
+    return jsonify({"entries": len(data["entries"])})
+
+
